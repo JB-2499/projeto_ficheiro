159bb67df681ec01a976c4a5bbc24707dc2f5598
João Barreto <Joo.bcsilva@upe.br>
2026-01-21 18:41:58 -0300

    Ler arquivos contidos na pasta imagens automaticamente. Adicionar funcionalidade aos botões.

--- projeto_ficheiro/src/Main.java
+++ projeto_ficheiro/src/Main.java
@@ -1,56 +1,7 @@
 import gui.Janela;
 
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         Janela janela = new Janela();
-        janela.setLayout(null);
-
-        //Label
-        ImageIcon imagem = new ImageIcon(janela.getClass().getResource("/gui/imagem.png"));
-        JLabel rotulo = new JLabel("Isso é um teste.");
-        rotulo.setIcon(imagem);
-
-        rotulo.setVerticalTextPosition(JLabel.TOP);
-        rotulo.setHorizontalTextPosition(JLabel.CENTER);
-        rotulo.setForeground(Color.DARK_GRAY);
-        rotulo.setIconTextGap(5);
-
-        rotulo.setBounds(60, 20, imagem.getIconWidth(), imagem.getIconHeight()+35);
-
-        Border border = BorderFactory.createLineBorder(Color.DARK_GRAY, 5);
-        rotulo.setBorder(border);
-
-        //Panel
-        JPanel painelCentral = new JPanel();
-        painelCentral.setBackground(Color.lightGray);
-        painelCentral.setLayout(null);
-
-        JPanel painelUp = new JPanel();
-        painelUp.setLayout(null);
-        painelUp.setBackground(Color.DARK_GRAY);
-        painelUp.setAlignmentY(Component.TOP_ALIGNMENT);
-
-        painelUp.setBounds(0, 0, 340, 30);
-        painelCentral.setBounds(0, 30, 340, 370);
-
-        //Button
-        JButton botao1 = new JButton("Anterior");
-        botao1.setFont(new Font("Arial", Font.BOLD, 11));
-        botao1.setBounds(5, 5, 80, 20);
-
-        JButton botao2 = new JButton("Próxima");
-        botao2.setFont(new Font("Arial", Font.BOLD, 11));
-        botao2.setBounds(240, 5, 80, 20);
-
-        painelUp.add(botao1);
-        painelUp.add(botao2);
-        painelCentral.add(rotulo);
-        janela.add(painelUp);
-        janela.add(painelCentral);
-        janela.setVisible(true);
     }
 }
--- projeto_ficheiro/src/gui/Janela.java
+++ projeto_ficheiro/src/gui/Janela.java
@@ -1,9 +1,19 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Janela extends JFrame {
+public class Janela extends JFrame{
+    private JButton botao1;
+    private JButton botao2;
+
+    private int indiceAtual = 0;
+    private List<ImageIcon> listaImagens;
+
     public Janela() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(340, 400);
@@ -13,5 +23,82 @@ public class Janela extends JFrame {
         this.setIconImage(icon.getImage());
 
         this.getContentPane().setBackground(Color.lightGray);
+
+        this.setLayout(null);
+
+        //Imagem
+        listaImagens = getListaImagens("projeto_ficheiro/src/gui/imagens");
+
+        //Label
+        JLabel rotulo = new JLabel("Isso é um teste.");
+        rotulo.setIcon(listaImagens.get(indiceAtual));
+
+        rotulo.setVerticalTextPosition(JLabel.TOP);
+        rotulo.setHorizontalTextPosition(JLabel.CENTER);
+        rotulo.setForeground(Color.DARK_GRAY);
+        rotulo.setIconTextGap(5);
+
+        rotulo.setBounds(60, 20, listaImagens.get(1).getIconWidth(), listaImagens.get(1).getIconHeight()+35);
+
+        Border border = BorderFactory.createLineBorder(Color.DARK_GRAY, 5);
+        rotulo.setBorder(border);
+
+        //Panel
+        JPanel painelCentral = new JPanel();
+        painelCentral.setBackground(Color.lightGray);
+        painelCentral.setLayout(null);
+
+        JPanel painelUp = new JPanel();
+        painelUp.setLayout(null);
+        painelUp.setBackground(Color.DARK_GRAY);
+
+        painelUp.setBounds(0, 0, 340, 30);
+        painelCentral.setBounds(0, painelUp.getHeight(), 340, 370);
+
+        //Button
+        this.botao1 = new JButton("Anterior");
+        botao1.addActionListener(e -> {
+            indiceAtual--;
+            if (indiceAtual < 0) {
+                indiceAtual = listaImagens.size() - 1;
+            }
+            rotulo.setIcon(listaImagens.get(indiceAtual));
+        });
+        botao1.setFont(new Font("Arial", Font.BOLD, 11));
+        botao1.setBounds(5, 5, 80, 20);
+        botao1.setBackground(Color.lightGray);
+        botao1.setForeground(Color.DARK_GRAY);
+
+        this.botao2 = new JButton("Próxima");
+        botao2.addActionListener(e -> {
+            indiceAtual = (indiceAtual + 1) % listaImagens.size();
+            rotulo.setIcon(listaImagens.get(indiceAtual));}
+        );
+        botao2.setFont(new Font("Arial", Font.BOLD, 11));
+        botao2.setBounds(240, 5, 80, 20);
+        botao2.setBackground(Color.lightGray);
+        botao2.setForeground(Color.DARK_GRAY);
+
+        painelUp.add(botao1);
+        painelUp.add(botao2);
+        painelCentral.add(rotulo);
+        this.add(painelUp);
+        this.add(painelCentral);
+        this.setVisible(true);
+    }
+
+    public List<ImageIcon> getListaImagens(String path) {
+        File pasta = new File(path);
+        File[] arquivos = pasta.listFiles();
+
+        List<ImageIcon> listaImagens = new ArrayList<>();
+
+        for (File f : arquivos) {
+            if (f.isFile() && f.getName().endsWith(".png")) {
+                listaImagens.add(new ImageIcon(f.getAbsolutePath()));
+            }
+        }
+
+        return listaImagens;
     }
 }
